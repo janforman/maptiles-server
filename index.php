9bd3d9dacb591e412207eb63d8e2cb4d5e50aaba
Jan Forman <48495095+janforman@users.noreply.github.com>
2021-07-21 11:10:20 +0200

    Beautify, minimize IO while serving images

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $basepath = '/home/';
 $mimetype = 'image/jpg';
 $image = 'jpg';
 
-function convhex($tileid) {
+function convhex($tileid)
+{
 	return sprintf("%08s", dechex($tileid));
 }
 //////////////////////////////////////////
@@ -17,32 +18,27 @@ $id = explode('/', $id[1]);
 $layer = $id[2];
 $directory = $id[3];
 $file = $id[4];
-if($id[3] == '') {
+if ($id[3] == '') {
 	// send JSON service descriptor
-	if(file_exists($basepath . $service[1] . 'json.txt')) {
+	if (file_exists($basepath . $service[1] . 'json.txt')) {
 		header('Cache-control: max-age=' . $expiresOffset);
-		if($_GET['callback'])
-			echo $_GET['callback'] . "(";
-		readfile($basepath . $service[1] . 'json.txt');
-		if($_GET['callback'])
-			echo ");";
+		if ($_GET['callback']) echo $_GET['callback'] . "(" . readfile($basepath . $service[1] . 'json.txt') . ");";
+		else readfile($basepath . $service[1] . 'json.txt');
 		exit;
 	} else {
 		header('HTTP/1.0 403 Forbidden');
-		exit('Service Not Found / Access Denied');
+		exit('Map Service not Found / Access Denied');
 	}
 }
-$tile = 'tiles/L' . sprintf("%02d", $layer). '/R' . convhex($directory). '/C' . convhex($file). '.' . $image;
-if(file_exists($basepath . $service[1] . $tile)) {
-	header('Cache-control: max-age=' . $expiresOffset);
-	header('Content-type: ' . $mimetype);
-	readfile($basepath . $service[1] . $tile);
-} else {
-	// image don't exist
+$tile = 'tiles/L' . sprintf("%02d", $layer) . '/R' . convhex($directory) . '/C' . convhex($file) . '.' . $image;
+
+header('Cache-control: max-age=' . $expiresOffset);
+header('Content-type: ' . $mimetype);
+
+if (!@readfile($basepath . $service[1] . $tile)) {
+	// image doesn't exist
 	header('Cache-control: max-age=' . $expiresOffset);
 	header('Content-type: image/jpeg');
 	header('Content-Length: ' . (string) filesize($basepath . '404.jpg'));
 	readfile($basepath . '404.jpg');
 }
-
-?>
